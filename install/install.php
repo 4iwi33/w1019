50f137f74a57afd51835ae6724ba9948cfecc962
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-08-07 20:39:36 +0300

    composer update

--- install/install.php
+++ install/install.php
@@ -22,12 +22,17 @@ $runner = new Runner(
         'password' => Config::MYSQL_PASSWORD
     ]));
 
-foreach (explode(";", file_get_contents('install/guests_book.sql')) as $value) {
-    try {
-        if (!empty($value)) {
-            $runner->runSQL($value);
-        }
-    } catch (Exception $e) {
-        echo $e->getMessage();
-    }
-}
+    $runner->setErrorHandler(function($mysqli, $sql){
+        echo "$mysqli->error\n";
+    });
+$runner->runScript(file_get_contents('install/guests_book.sql'));
+
+// foreach (explode(";", file_get_contents('install/guests_book.sql')) as $value) {
+//     try {
+//         if (!empty($value)) {
+//             $runner->runSQL($value);
+//         }
+//     } catch (Exception $e) {
+//         echo $e->getMessage();
+//     }
+// }
